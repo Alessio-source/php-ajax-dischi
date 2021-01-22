3d94efbead1fda19063743a3b1cd39d4c5fd2242
Alessio Pancia <alessiop2801@gmail.com>
2021-01-22 14:54:17 +0100

    css

--- index.php
+++ index.php
@@ -10,11 +10,20 @@
   <head>
     <meta charset="utf-8">
 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 
     <title>PHP dischi</title>
   </head>
   <body>
+    <header>
+      <div class="container">
+        <img src="css/img/spotify.png" alt="">
+      </div>
+
+    </header>
+
     <?php include __DIR__ . "/partials/main.php" ?>
   </body>
 </html>
--- partials/main.php
+++ partials/main.php
@@ -1,15 +1,17 @@
 
 <main>
-  <?php
-    foreach ($data as $key => $value) {
-   ?>
-      <div class="disc">
-        <img src="<?php echo $value['poster']; ?>" alt="">
-        <p><?php echo $value['title']; ?></p>
-        <p><?php echo $value['author']; ?></p>
-        <p><?php echo $value['year']; ?></p>
-      </div>
-  <?php
-    }
-   ?>
+  <div class="container">
+    <?php
+      foreach ($data as $key => $value) {
+     ?>
+        <div class="disc">
+          <img src="<?php echo $value['poster']; ?>" alt="">
+          <p><strong><?php echo $value['title']; ?></strong></p>
+          <p><?php echo $value['author']; ?></p>
+          <p><?php echo $value['year']; ?></p>
+        </div>
+    <?php
+      }
+     ?>
+  </div>
 </main>
